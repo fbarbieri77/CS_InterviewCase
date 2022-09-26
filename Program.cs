3f370b4068f2bea5d600a9acad044e87184d972d
fbarbieri77 <barbieri.f@gmail.com>
2022-09-26 15:40:21 -0300

    2022-sep-26

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
 using System.Globalization;
 
 namespace ConsoleApp_CSCase
@@ -11,57 +13,167 @@ namespace ConsoleApp_CSCase
         bool IsPoliticallyExposed { get; }
     }
 
+    interface ICategory
+    {
+        string Name { get; }
+        bool Identify(ITrade trade);
+    }
+
+    enum EnumAllCategories
+    {
+        EXPIRED,
+        MEDIUMRISK,
+        HIGHRISK,
+        PEP,
+        NA,
+    }
+
     class Program
     {
+        public class Expired : ICategory
+        {
+            private readonly DateTime referenceDate;
+
+            public string Name
+            {
+                get
+                {
+                    return "EXPIRED";
+                }
+            }
+            public Expired (string _referenceDate)
+            {
+                referenceDate = DateTime.ParseExact(_referenceDate, "MM/dd/yyyy", CultureInfo.InvariantCulture);
+            }
+
+            public bool Identify(ITrade trade)
+            {
+                return referenceDate > trade.NextPaymentDate.AddDays(30);
+            }
+        }
+
+        public class MediumRisk : ICategory
+        {
+            public string Name
+            {
+                get
+                {
+                    return "MEDIUMRISK";
+                }
+            }
+
+            public bool Identify(ITrade trade)
+            {
+                return trade.Value > 1000000 && trade.ClientSector == "Public";
+            }
+        }
+
+        public class HighRisk : ICategory
+        {
+            public string Name 
+            {
+                get
+                {
+                    return "HIGHRISK";
+                }
+            }
+
+            public bool Identify(ITrade trade) 
+            {
+                return trade.Value > 1000000 && trade.ClientSector == "Private";
+            }
+        }
+
+        public class PEP : ICategory
+        {
+            public string Name
+            {
+                get
+                {
+                    return "PEP";
+                }
+            }
+
+            public bool Identify(ITrade trade)
+            {
+                return trade.IsPoliticallyExposed;
+            }
+        }
+
+        public class NotCategorised : ICategory
+        {
+            public string Name
+            {
+                get
+                {
+                    return "Not categorised";
+                }
+            }
+
+            public bool Identify(ITrade trade)
+            {
+                return true;
+            }
+        }
+
+        class GetCategoryFactory
+        {
+            public ICategory GetCategory(string categoryType, string referenceDate)
+            {
+                if (categoryType == null)
+                {
+                    return null;
+                }
+                else if (categoryType == EnumAllCategories.EXPIRED.ToString())
+                {
+                    return new Expired(referenceDate);
+                }
+                else if (categoryType == EnumAllCategories.MEDIUMRISK.ToString())
+                {
+                    return new MediumRisk();
+                }
+                else if (categoryType == EnumAllCategories.HIGHRISK.ToString())
+                {
+                    return new HighRisk();
+                }
+                else if (categoryType == EnumAllCategories.PEP.ToString())
+                {
+                    return new PEP();
+                }
+                else if (categoryType == EnumAllCategories.NA.ToString())
+                {
+                    return new NotCategorised();
+                }
+
+                return null;
+            }
+
+            public List<ICategory> GetAllCategories(string referenceDate)
+            {
+                List<ICategory> categories = new List<ICategory>();
+                foreach (string categoryName in Enum.GetNames(typeof(EnumAllCategories)))
+                {
+                    categories.Add(this.GetCategory(categoryName, referenceDate));
+                }
+                return categories;
+            }
+        }
+
         class Trade : ITrade
         {
-            public DateTime ReferenceDate { get; }
             public double Value { get; }
             public string ClientSector { get; }
             public DateTime NextPaymentDate { get; }
             public bool IsPoliticallyExposed { get; }
+
             public Trade(string _referenceDate, string[] parameters)
             {
-                /* _referenceDate: trade reference date in "MM/dd/yyyy"
-                 * parameters[x]
-                 * x = 0: trade amount
-                 * x = 1: client's sector
-                 * x = 2: next pending payment in "MM/dd/yyyy"
-                 * x = 3: true if it is a PEP (politically exposed person)
-                 */
-
-                ReferenceDate = DateTime.ParseExact(_referenceDate, "MM/dd/yyyy", CultureInfo.InvariantCulture);
                 Value = double.Parse(parameters[0]);
                 ClientSector = parameters[1];
                 NextPaymentDate = DateTime.ParseExact(parameters[2], "MM/dd/yyyy", CultureInfo.InvariantCulture);
-                IsPoliticallyExposed = parameters[3] == "true"; // there must be a 4th column in the input file with either true or false value
+                IsPoliticallyExposed = parameters[3] == "true";
             }
 
-            public string Classify()
-            {
-                if (ReferenceDate > NextPaymentDate.AddDays(30))
-                {
-                    return "EXPIRED";
-                }
-                else if (Value > 1000000)
-                {
-                    if (ClientSector == "Private")
-                    {
-                        return "HIGHRISK";
-                    }
-                    else if  (ClientSector == "Public")
-                    {
-                        return "MEDIUMRISK";
-                    }
-                }
-                else if (IsPoliticallyExposed)
-                {
-                    return "PEP";
-                }
-                
-                return "na";
-
-            }
         }
 
         static void Main(string[] args)
@@ -77,17 +189,23 @@ namespace ConsoleApp_CSCase
             }
 
             // classifiy trades
+            GetCategoryFactory categoryFactory = new GetCategoryFactory();
+            List<ICategory> categories = categoryFactory.GetAllCategories(referenceDate);
             for (int i = 2; i < lines.Length; i++)
             {
                 string[] tradeData = lines[i].Split(" ");
                 Trade trade = new Trade(referenceDate, tradeData);
-                Console.WriteLine(trade.Classify());
+
+                foreach (ICategory category in categories)
+                {
+                    if (category.Identify(trade))
+                    {
+                        Console.WriteLine(category.Name);
+                        break;
+                    }
+                }
             }
         }
+
     }
 }
-
-/* Question 2
- * In order to have an extensible design allowing new categories to be added easily, I would require the categories to be passed as an array of strings. 
- * In this way, I just need to include the new category in the interface ITrade and implement it in the class Trade recalling I know the position of each category in parameters[].
- */
